5af5c28bb9bb8b08aca47bf4ff9cf06d4a5d27db
Yauheni <gek0305@gmail.com>
2022-03-03 15:09:33 +0300

    new method in main_page, base_page and test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -8,3 +8,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
