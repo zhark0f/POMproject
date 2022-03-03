03d13b5cf949707fa07a8dbb6506b014876198be
Yauheni <gek0305@gmail.com>
2022-03-03 14:40:37 +0300

    new files: test_main_page.py and requirement.txt

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: cs, da, de, en-bg, el, es, fi, \
+                                            fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans.\
+                                            Default is ru")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart chrome browser for test..")
+    options = Options()
+    language_of_browser = request.config.getoption("language")
+    options.add_experimental_option('prefs', {'intl.accept_languages': language_of_browser})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+
---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+from .pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    page.open()                      # открываем страницу
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
