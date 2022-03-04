d108a99a02bcb734b26f134c4b65a26e5da5a0d8
Yauheni <gek0305@gmail.com>
2022-03-04 21:44:22 +0300

    modified test_page.py , corrected name of method

--- test_main_page.py
+++ test_main_page.py
@@ -15,7 +15,7 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
     
-def test_guest_should_be_login_page(browser):
+def test_guest_should_be_see_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
