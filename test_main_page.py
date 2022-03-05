7b6ef03302f7e8e862d4c9da216d1294d88faf74
Yauheni <gek0305@gmail.com>
2022-03-05 13:18:37 +0300

    add new lines to 2 files: i can go to another page at website

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
